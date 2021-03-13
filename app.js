008f63a78e10cd763c96b3eb2563b8d485e80ab0
Vijayaalayan A <vijayaalayan1234@gmail.com>
2021-03-14 00:03:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,14 +13,15 @@ const server = http.createServer((req,res) =>{
         req.on('data',(chunk)=>{
             body.push(chunk);
         });
-        req.on('end',()=>{
+        return req.on('end',()=>{
             const parsedBody = Buffer.concat(body).toString();
             const message = parsedBody.split('=')[1];
             fs.writeFileSync('message.txt', message);
+            res.statusCode = 302;
+            res.setHeader('Location', '/');
+            return res.end();
         });
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
+        
     }
     console.log(res);
     res.setHeader('Content-Type','text/html');
