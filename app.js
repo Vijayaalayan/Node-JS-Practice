6a0cb3f50a8d8a11242cd8669845836d7173ff38
Vijayaalayan A <vijayaalayan1234@gmail.com>
2021-03-14 12:28:37 +0530

    routing

--- app.js
+++ app.js
@@ -1,39 +1,8 @@
 const http = require('http')
-const fs = require('fs')
-const server = http.createServer((req,res) =>{
-    const url = req.url;
-    const method = req.method;
-    if(url === '/'){
-        res.write('<html><head><title>Form</title></head>')
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body></html>');
-        return res.end();
-    }
-    if(url === '/message' && method === 'POST'){
-        const body = [];
-        req.on('data',(chunk)=>{
-            body.push(chunk);
-        });
-        return req.on('end',()=>{
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-            // fs.writeFileSync('message.txt', message);
-            // res.statusCode = 302;
-            // res.setHeader('Location', '/');
-            // return res.end();
 
-            // writeFileDync block the code
-            fs.writeFile('message.txt',message,err=>{
-                res.statusCode = 302;
-                res.setHeader('Location', '/');
-                return res.end();
-            });
-        });
-        
-    }
-    console.log(res);
-    res.setHeader('Content-Type','text/html');
-    res.write('<html><head><title>Node</title></head><body>Helo World from Node JS</body></html>');
-    res.end();
-});
+const routes = require('./routes')
+
+console.log(routes.exampleText)
+const server = http.createServer(routes.handler);
 
 server.listen(3000);
