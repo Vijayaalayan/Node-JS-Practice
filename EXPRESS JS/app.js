0711faa27a154d140d050a977788955ed5d0829b
Vijayaalayan A <vijayaalayan1234@gmail.com>
2021-03-15 23:08:01 +0530

    initial express commit

--- EXPRESS JS/app.js
+++ EXPRESS JS/app.js
@@ -1,5 +1,20 @@
 const http = require('http')
 
-const server = http.createServer();
+const express = require('express')
+
+const app = express();
+
+app.use((req,res,next)=>{
+    console.log("In First Middleware");
+    // If next function is not called the use function dies and it'll not pass along.
+    next();
+});
+
+app.use((req,res,next)=>{
+    console.log("In Second Middleware");
+    res.send('<h1>ExpressJS</h1>')
+});
+
+const server = http.createServer(app);
 
 server.listen(3000);
