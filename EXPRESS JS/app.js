4fd970a31a80e107cccb4278ae05ef9d968c0c41
Vijayaalayan A <vijayaalayan1234@gmail.com>
2021-03-16 12:47:06 +0530

    handling routes

--- EXPRESS JS/app.js
+++ EXPRESS JS/app.js
@@ -1,20 +1,25 @@
-const http = require('http')
-
 const express = require('express')
 
 const app = express();
 
-app.use((req,res,next)=>{
-    console.log("In First Middleware");
-    // If next function is not called the use function dies and it'll not pass along.
+// app.use((req,res,next)=>{
+//     console.log("In First Middleware");
+//     // If next function is not called the use function dies and it'll not pass along.
+//     next();
+// });
+
+app.use('/',(req,res,next)=>{
+    console.log('This always runs');
     next();
 });
 
-app.use((req,res,next)=>{
+app.use('/add-page',(req,res,next) =>{
+    res.send('Add Page');
+});
+
+app.use('/',(req,res,next)=>{
     console.log("In Second Middleware");
     res.send('<h1>ExpressJS</h1>')
 });
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
