844ae1170514d0c21db20bb7f5008c3839daa516
Vijayaalayan A <vijayaalayan1234@gmail.com>
2021-03-22 11:29:06 +0530

    path filtering

--- EXPRESS JS/app.js
+++ EXPRESS JS/app.js
@@ -7,7 +7,7 @@ const shopRouter = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.use(adminRouter);
+app.use('/admin',adminRouter);
 app.use(shopRouter);
 app.use((req,res,next)=>{
     res.status(404).send('<h1>Page Not Found</h1>')
--- EXPRESS JS/routes/admin.js
+++ EXPRESS JS/routes/admin.js
@@ -5,10 +5,11 @@ const app = express();
 
 //use it with either post or get as in like app.get or app.post to parse
 router.get('/add-food',(req,res,next) =>{
-    res.send('<form action="/food" method="POST"><input type="text" name="title"><button type="submit">Add Food</button></form>');
+    res.send('<form action="/admin/add-food" method="POST"><input type="text" name="title"><button type="submit">Add Food</button></form>');
 });
 
-router.post('/food',(req,res,next)=>{
+//same path can be used for different method
+router.post('/add-food',(req,res,next)=>{
     console.log(req.body.title);
     res.redirect('/');
 });
